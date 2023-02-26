c3fd05a5d69ba16fc8a3d991f331ed0f99cd7b7e
Kiko <kikoguirado@gmail.com>
2023-02-26 01:16:54 +0100

    first service working

--- src/app/demo/components/categories/electronics/electronics.component.ts
+++ src/app/demo/components/categories/electronics/electronics.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/demo/interfaces/product';
 
 @Component({
   selector: 'app-electronics',
   templateUrl: './electronics.component.html',
   styleUrls: ['./electronics.component.scss']
 })
-export class ElectronicsComponent {
+export class ElectronicsComponent implements OnInit {
+
+  constructor(public http: HttpClient) {
+
+  }
+
+  products!: Product[]
+
+  ngOnInit(): void {
+    this.http.get<Product[]>('https://fakestoreapi.com/products/category/electronics').subscribe(
+      res => {
+        this.products = res
+        console.log(res)
+      }
+    )
+  }
 
 }
